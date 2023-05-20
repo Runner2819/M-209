a3a836195e75e88a6c94aa936fea2d514da6080d
Runner2819 <58637130+Runner2819@users.noreply.github.com>
2023-05-20 21:07:42 +0300

    Update m209.py

--- m209.py
+++ m209.py
@@ -1,19 +1,15 @@
-# m209 : The base Class of M209
+import random
 
-import string
-
-M209_ALPHABET_LIST = string.ascii_uppercase
-M209_ALPHABET_SET = set(string.ascii_uppercase)
-CIPHER_TABLE = list(reversed(string.ascii_uppercase))
+CIPHER_TABLE = "ZYXWVUTSRQPONMLKJIHGFEDCBA"
 
 # historically accurate
 WHEEL_DATA = [
-    ("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 15),
-    ("ABCDEFGHIJKLMNOPQRSTUVXYZ", 14),
-    ("ABCDEFGHIJKLMNOPQRSTUVX", 13),
-    ("ABCDEFGHIJKLMNOPQRSTU", 12),
-    ("ABCDEFGHIJKLMNOPQRS", 11),
-    ("ABCDEFGHIJKLMNOPQ", 10),
+    ("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 15),  # 26
+    ("ABCDEFGHIJKLMNOPQRSTUVXYZ", 14),  # 25
+    ("ABCDEFGHIJKLMNOPQRSTUVX", 13),  # 23
+    ("ABCDEFGHIJKLMNOPQRSTU", 12),  # 21
+    ("ABCDEFGHIJKLMNOPQRS", 11),  # 19
+    ("ABCDEFGHIJKLMNOPQ", 10),  # 17
 ]
 
 
@@ -41,6 +37,13 @@ class Wheel:
             self.pins[n] = 1
 
         self.effective_pins = effective_pins
+
+    def random_pins(self):
+        effect_pins = ""
+        for i in range(len(self.letters)):
+            if random.choice([0, 1]):
+                effect_pins += self.letters[i]
+        self.set_pins(effect_pins)
 
     def rotate(self, steps=1):
         self.pos = (self.pos + steps) % self.size
@@ -80,6 +83,12 @@ class Drum:
             for j in range(int(repeat)):
                 self.lugs.append(list(map(int, lug_pair.split('-'))))
 
+    def random_lugs(self):
+        lugs = ""
+        for i in range(27):
+            lugs += '-'.join(random.choices("0123456", k=2)) + ' '
+        self.set_lugs(lugs)
+
     def rotate(self, pins):
         count = 0
         for lug_pair in self.lugs:
@@ -117,7 +126,11 @@ class M209:
             self.wheels[n].set_pos(s[n])
 
     def prepare(self, ext_key, letter):
-        self.set_key_wheels(ext_key)
+        try:
+            self.set_key_wheels(ext_key)
+        except:
+            print("WRONG EXTERNAL KEY")
+            return 1
         tmp = self.encrypt(letter * 12)
         self.reset()
         wh_num = 0
@@ -127,6 +140,19 @@ class M209:
                 self.set_key_wheel(wh_num, tmp[ind])
                 wh_num += 1
             ind += 1
+        return 0
+
+    def random_settings(self):
+        ext_key = ""
+        letter = random.choice(CIPHER_TABLE)
+        for wh in self.wheels:
+            ext_key += random.choice(wh.letters)
+            wh.random_pins()
+        self.drum.random_lugs()
+
+        while self.prepare(ext_key, letter) != 0:
+            self.reset()
+        return ext_key, letter
 
     def encrypt(self, plaintext="A"):
         ciphertext = ""
@@ -146,6 +172,15 @@ class M209:
             plaintext += new_c
         return plaintext
 
+    def print_settings(self):
+        print("pins:", end=" ")
+        for wh in self.wheels:
+            print(wh.effective_pins, end=", ")
+        print("\nlugs:", end=" ")
+        for lugs in self.drum.lugs:
+            print('-'.join(map(str, lugs)), end=' ')
+        print("")
+
     def _cipher(self, c):
         pins = [wh.is_effective() for wh in self.wheels]
         count = self.drum.rotate(pins)
@@ -161,3 +196,33 @@ class M209:
         for wh in self.wheels:
             wh.rotate(-self.letter_counter)
         self.letter_counter = 0
+
+
+def create_machine():
+    machine = M209()
+    action = input("Random settings?(Y/n)")
+    if action == "Y":
+        ext_key, letter = machine.random_settings()
+        print("External key -", ext_key)
+        print("Letter for internal key -", letter)
+        machine.print_settings()
+    else:
+        lugset = input("enter lug settings ")
+        pinset = [input("enter pin settings for wheel " + str(i)) for i in range(6)]
+        machine = M209(lugset, pinset)
+        ext_key = input("enter external key ")
+        letter = input("enter random letter ")
+        machine.prepare(ext_key, letter)
+    return machine
+
+
+def machine_operation(machine):
+    action = input("Action(enc/dec/res)")
+    if action == "enc":
+        print(machine.encrypt(input(">")))
+    elif action == "dec":
+        print(machine.decrupt(input(">")))
+    elif action == "res":
+        machine.reset()
+    else:
+        print("Unknown action")
